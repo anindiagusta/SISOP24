b69d2a04bdaaea13aa3995b6db786cc7909762d6
Anindia Agusta <141204784+anindiagusta@users.noreply.github.com>
2024-11-10 19:17:36 +0700

    Add files via upload

---
+++ DontOpenJustRunTPM3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COUNT=0
+
+while [ $COUNT -le 100 ]; do
+        echo "Downloading Virus.exe... ($COUNT/100%)"
+        sleep 0.5
+        let COUNT=COUNT+1
+
+done
+
+echo "Virus.exe Downloaded Successfully!"
+sleep 5
+echo "Your PC ran into problem, please wait..."
+sleep 10
+echo "WARNING! YOUR COMPUTER IS INFECTED WITH VIRUSES!"
+sleep 10
+echo "RASL: Bercyandaa ciaa panik yaa"
+sleep 5
+echo "RASL: Selamat telah berhasil menginstall virus! xixixi"
+sleep 1
+echo "Untuk Tugas Pendahuluan Modul 3 Sistem Operasi bisa diakses di sini ya!"
+echo " "
+echo "Kelas Reguler       : https://bit.ly/TPSisop24Modul3Reguler"
+echo " "
+echo "International Class : https://bit.ly/TPSisop24Modul3International"
+echo "-----------------------------------------------------------------"
+echo "Tenang bukan virus kok hehe, rill peace peace *-*"
+echo "Selamat Mengerjakan!"
